dcf4b0ca086ddf25cd7479998ab086cb9866af3d
CamC <cuellarc660@gmail.com>
2018-11-22 01:17:02 -0800

    Added deal pair and deal add cards functions, and made sure the cards cannot repeat

--- Project.py
+++ Project.py
@@ -11,5 +11,21 @@ import math, random
 cardTypes = ['s','c','h','d'] #The suits for a card are: 's' for spades, 'c' for clubs, 'h' for hearts, or 'd' for diamonds
 cardVals = [str(i) for i in range(2,11)] + ['J','Q','K','A'] #The values for a card are numbers 2-10, 'J' for Jack, 'Q' for Queen, 'K' for King, or 'A' for Ace
 cardsList = [s + c for s in cardTypes for c in cardVals] #The list of all 52 possible cards in a deck are compiled into a list
+#print(cardsList)
 
-print(cardsList)
+def dealPair(currentPair = ''):
+
+    availableCards = [e for e in cardsList if e not in currentPair]
+    newCard1, newCard2 = random.sample(availableCards,2)
+
+    return([newCard1, newCard2])
+
+def addCards(currentPair):
+    
+    availableCards = [e for e in cardsList if e not in currentPair]
+    deck = random.sample(availableCards, 5)
+    return(deck)
+
+pair = dealPair()
+print('Dealt hand: ' + str(pair))
+print('Dealt deck: ' + str(addCards(pair)))
