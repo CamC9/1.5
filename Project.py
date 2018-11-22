d8c22c575788f8f05e2f5de6964d86374d4ccd2c
CamC <cuellarc660@gmail.com>
2018-11-22 00:13:48 -0800

    Chose idea for project and finished creation of the list for deck of cards

--- Project.py
+++ Project.py
@@ -1,3 +1,15 @@
-test = 'g'
+'''
+This program will simulate a poker game, but with one player and a dealer,
+where the objective of the game is to gain points by getting good combinations
+of cards (hands)
 
-print(test)
+It's possible we can expand the game to support playing against a computer
+'''
+
+import math, random
+
+cardTypes = ['s','c','h','d'] #The suits for a card are: 's' for spades, 'c' for clubs, 'h' for hearts, or 'd' for diamonds
+cardVals = [str(i) for i in range(2,11)] + ['J','Q','K','A'] #The values for a card are numbers 2-10, 'J' for Jack, 'Q' for Queen, 'K' for King, or 'A' for Ace
+cardsList = [s + c for s in cardTypes for c in cardVals] #The list of all 52 possible cards in a deck are compiled into a list
+
+print(cardsList)
