d264b1fc76edb1d8b06fae61684086a976674e1c
Cameron Cuellar <cuellarc660@gmail.com>
2018-12-09 18:08:29 -0800

    Finished ability to return winning hand for all combinations, and fixed all bugs.  TODO: Add ability to check for straights is Ace is the lower bound, Add playing options (like bettting, checking, raising, and folding), Add Computer AI, and add visual elements to the game

--- Project.py
+++ Project.py
@@ -16,7 +16,6 @@ def dealPair(currentPair = ''): #Randomly generates a pair of cards for the play
 
     availableCards = [e for e in cardsList if e not in currentPair]
     newCard1, newCard2 = random.sample(availableCards,2)
-
     return([newCard1, newCard2])
 
 
@@ -33,8 +32,8 @@ def combine(pair, table): #Combines the player's pair cards and the table cards 
 
     totalCards = pair + table
     totalCards.sort()
-    print('Total cards: ' + str(reorder(totalCards)))
-    return(totalCards)
+    printReturn = 'Total cards: ' + str(reorder(totalCards))
+    return(totalCards, printReturn)
 
 
 
@@ -104,7 +103,55 @@ def RepresentsInt(s):
 
 
 
+def convert(letter):
+    conversion = {'J':11,
+                  'Q':12,
+                  'K':13,
+                  'A':14}
+    
+    if letter == 'J' or letter == 'Q' or letter == 'K' or letter == 'A':
+        return(conversion[letter])
+
+    else:
+        return('z')
+
+
 def isStraight(cards):
+
+    cards = reorder(cards)
+    winningHand = []
+    values = returnValues(cards)
+    values = list(set(values))
+
+    for i in range(len(values)-4):
+        if values[-(i+1)] - values[-(i+5)] == 4:
+            for j in range(5):
+                winningHand += [card for card in cards if (str(values[(-(i+5))+j]) in card or values[(-(i+5))+j] == checkValue(card[-1]))]
+
+            if len(winningHand) > 5:
+
+                if winningHand[-3][-1] == winningHand[-1][-1]:
+                    winningHand.remove(winningHand[-3])
+                    winningHand.remove(winningHand[-2])
+                
+                elif winningHand[-2][-1] == winningHand[-1][-1]:
+                    winningHand.remove(winningHand[-2])
+
+                for k in range(len(winningHand)):
+                    if k < 4:
+                        if winningHand[k+1][-1] == winningHand[k][-1]:
+                            if winningHand[k+2][-1] == winningHand[k][-1]:
+                                winningHand.remove(winningHand[k+2])
+                                winningHand.remove(winningHand[k+1])
+                            else:
+                                winningHand.remove(winningHand[k+1])
+            return[True,winningHand]
+    
+    return(False)
+
+
+
+def isStraightSafe(cards):
 
     results = []
     values = returnValues(cards)
@@ -114,21 +161,52 @@ def isStraight(cards):
         if values[-(i+1)] - values[-(i+5)] == 4:
             for j in range(5):
                 results += [values[(-(i+5))+j]]
-            return[True,results]
+            return[True]
     return(False)
 
 
 
 def isFlush(cards):
 
-    results = [card for i, card in enumerate(cards) if cards[i-1][0] == cards[i][0]]
+    suitOrder = ['s','h','c','d']
+    winningHand = []
+    occ = {'s':[],
+           'h':[],
+           'c':[],
+           'd':[]}
+        
+    for card in cards:
+        if card[0] == 's':
+            occ['s'] += [card]
+        elif card[0] == 'h':
+            occ['h'] += [card]
+        elif card[0] == 'c':
+            occ['c'] += [card]
+        elif card[0] == 'd':
+            occ['d'] += [card]
 
-    if len(results) == 4 and results[0][0] == results[1][0] == results[2][0] == results[3][0]: #Not sure how to make this statement more compact
-        results += [cards[(cards.index(results[0]))-1]]
-        results = reorder(results)
-        return[True,results]    
-    else:
-        return(False)
+    for suit in suitOrder:
+        if len(occ[suit]) >= 5:
+            winningHand = reorder(occ[suit])
+            while len(winningHand) > 5:
+                winningHand.remove(winningHand[0])
+            return(True,winningHand)
+
+    return(False)
+
+
+
+def isRoyalFlush(cards):
+    if isFlush(cards):
+        if isStraight(isFlush(cards)[-1]) and isFlush(cards)[-1][-1][-1] == 'A':
+            return(True,isStraight(isFlush(cards)[-1])[-1])
+
+
+
+def isStraightFlush(cards):
+    if isFlush(cards):
+        if isStraight(isFlush(cards)[-1]):
+            return(True,isStraight(isFlush(cards)[-1])[-1])
 
 
 
@@ -150,26 +228,45 @@ def checkOccurrences(cards):
     for i, value in enumerate(values):
         if i < 5:
             if values[i+2] == values[i-1]:
-                occurrences = 3
+                occurrences += 3
                 for j in range(-1,3):
-                    newList = [values[i+j]]
+                    newList += [values[i+j]]
                 break
             elif values[i+2] == value:
                 occurrences += 2
                 for j in range(3):
                     newList += [values[i+j]]
-            elif (values[i+1] == value and values[i+2] != value and values[i-1] != value) or (values[i+1] == values[i+2] and values[i+1] != value and i == 4):
+            elif (values[i+1] == value and values[i+2] != value and values[i-1] != value) :
                 occurrences += 1
                 for j in range(2):
                     newList += [values[i+j]]
+            elif values[i+1] == values[i+2] and values[i+1] != value and i == 4:
+                occurrences += 1
+                for j in range(2):
+                    newList += [values[(i+1)+j]]
 
-    if occurrences > 2 and len(newList) == 4:
+    
+
+    
+    if occurrences > 2 and len(newList) == 7 and newList[0] == newList[-1]:
         is4OfKind = True
+
+    elif len(newList) >= 9:
+        is4OfKind = True
+        newList = removeAll(newList,newList[0])
 
     elif occurrences > 2 and len(newList) == 5:
         isFullHouse = True
 
-    elif occurrences > 2:
+    elif occurrences > 2 and len(newList) == 7:
+        isFullHouse = True
+        newList = removeAll(newList,newList[0])
+
+    elif occurrences > 2 and len(newList) == 6 and newList[0] == newList[2]:
+        isFullHouse = True
+        newList.remove(newList[0])
+
+    elif occurrences > 2 and len(newList) == 6 and newList[0] != newList[2]:
         isTwoPair = True
         newList = removeAll(newList,newList[0])
 
@@ -188,22 +285,73 @@ def checkOccurrences(cards):
         is3OfKind = False
         is4OfKind = False
         isFullHouse = False
-        isHighCard = True
+
+        if not isStraightSafe(cards) and not isFlush(cards):
+            isHighCard = True
     
     for num in list(set(newList)):
         for card in cards:
             if str(num) in card or num == checkValue(card[-1]):
                 winningHand += [card]
                 winningHand = reorder(winningHand)
-    
+
+    if len(winningHand) > 5:
+        for i in range(len(winningHand)-5):
+            winningHand.remove(winningHand[i])
+
     for item in winningHand:
         reordered = removeAll(reordered,item)
 
-    for i in range(5-len(winningHand)):
-        winningHand += [reordered[-1-i]]
+    if len(winningHand) < 5:
+        for i in range(5-len(winningHand)):
+            winningHand += [reordered[-1-i]]
 
     winningHand = reorder(winningHand)
     return(winningHand)
+
+
+
+def returnWinningHand(currentPair, currentTable, points):
+
+    global isTwoPair
+    global isPair
+    global is3OfKind
+    global is4OfKind
+    global isFullHouse
+    global isHighCard
+
+    isPair = False
+    isTwoPair = False
+    is3OfKind = False
+    is4OfKind = False
+    isFullHouse = False
+    isHighCard = False
+
+    winningHand3 = []
+    winningHand4 = []
+
+    totalCards = combine(currentPair, currentTable)[0]
+
+    if isRoyalFlush(totalCards):
+        winningHand3 = isRoyalFlush(totalCards)[-1]
+        return(winningHand3)
+
+    elif isStraightFlush(totalCards):
+        winningHand3 = isStraightFlush(totalCards)[-1]
+        return(winningHand3)
+
+    elif isFlush(totalCards):
+        winningHand3 = isFlush(totalCards)[-1]
+        return(winningHand3)
+    
+    elif isStraight(totalCards):
+        winningHand3 = isStraight(totalCards)[-1]
+        return(winningHand3)
+
+    winningHand4 = checkOccurrences(totalCards)
+    return(winningHand4)
+
+
 
 def checkHand(currentPair, currentTable, points):
 
@@ -221,50 +369,63 @@ def checkHand(currentPair, currentTable, points):
     isFullHouse = False
     isHighCard = False
 
-    totalCards = combine(currentPair, currentTable)
+    winningHand1 = []
+    winningHand2 = []
 
-    if isFlush(totalCards):
+    totalCards = combine(currentPair, currentTable)[0]
+    print(combine(currentPair, currentTable)[1])
 
-        if isStraight(isFlush(totalCards)[-1]) and isFlush(totalCards) and isFlush(totalCards)[-1][-1][-1] == 'A':
-            points += pointSystem['royalFlush']
-            print('')
-            print('You got a royal flush! +' + str(pointSystem['royalFlush']) + ' points')
-            print('Total points: ' + str(points))
-            print('')
-            return(points)
+    if isRoyalFlush(totalCards):
+        winningHand1 = isRoyalFlush(totalCards)[-1]
+        points += pointSystem['royalFlush']
+        print('')
+        print('Winning Hand: ' + str(winningHand1))
+        print('')
+        print('You got a royal flush! +' + str(pointSystem['royalFlush']) + ' points')
+        print('Total points: ' + str(points))
+        print('')
+        return(points)
 
-        if isStraight(isFlush(totalCards)[-1]) and isFlush(totalCards):
-            points += pointSystem['straightFlush']
-            print('')
-            print('You got a straight flush! +' + str(pointSystem['straightFlush']) + ' points')
-            print('Total points: ' + str(points))
-            print('')
-            return(points)
+    elif isStraightFlush(totalCards):
+        winningHand1 = isStraightFlush(totalCards)[-1]
+        points += pointSystem['straightFlush']
+        print('')
+        print('Winning Hand: ' + str(winningHand1))
+        print('')
+        print('You got a straight flush! +' + str(pointSystem['straightFlush']) + ' points')
+        print('Total points: ' + str(points))
+        print('')
+        return(points)
 
+    elif isFlush(totalCards):
+        winningHand1 = isFlush(totalCards)[-1]
         points += pointSystem['flush']
+        print('')
+        print('Winning Hand: ' + str(winningHand1))
         print('')
         print('You got a flush! +' + str(pointSystem['flush']) + ' points')
         print('Total points: ' + str(points))
-        print(isFlush(totalCards))
         print('')
         return(points)
     
-    if isStraight(totalCards):
+    elif isStraight(totalCards):
+        winningHand1 = isStraight(totalCards)[-1]
         points += pointSystem['straight']
         print('')
+        print('Winning Hand: ' + str(winningHand1))
+        print('')
         print('You got a straight! +' + str(pointSystem['straight']) + ' points')
+        print
         print('Total points: ' + str(points))
-        print(isStraight(totalCards))
         print('')
         return(points)
 
-    winningHand = checkOccurrences(totalCards)
+    winningHand2 = checkOccurrences(totalCards)
 
-    if isPair or isTwoPair or is3OfKind or is4OfKind or isFullHouse or isHighCard:
-        print('Winning Hand: ' + str(winningHand))
-    
     if is4OfKind:
         points += pointSystem['4ofKind']
+        print('')
+        print('Winning Hand: ' + str(winningHand2))
         print('')
         print('You got a 4 of a kind! +' + str(pointSystem['4ofKind']) + ' points')
         print('Total points: ' + str(points))
@@ -274,6 +435,8 @@ def checkHand(currentPair, currentTable, points):
     if isFullHouse:
         points += pointSystem['fullHouse']
         print('')
+        print('Winning Hand: ' + str(winningHand2))
+        print('')
         print('You got a full house! +' + str(pointSystem['fullHouse']) + ' points')
         print('Total points: ' + str(points))
         print('')
@@ -281,6 +444,8 @@ def checkHand(currentPair, currentTable, points):
 
     if is3OfKind:
         points += pointSystem['3ofKind']
+        print('')
+        print('Winning Hand: ' + str(winningHand2))
         print('')
         print('You got a 3 of a kind! +' + str(pointSystem['3ofKind']) + ' points')
         print('Total points: ' + str(points))
@@ -290,6 +455,8 @@ def checkHand(currentPair, currentTable, points):
     if isTwoPair:
         points += pointSystem['twoPair']
         print('')
+        print('Winning Hand: ' + str(winningHand2))
+        print('')
         print('You got a two pair! +' + str(pointSystem['twoPair']) + ' points')
         print('Total points: ' + str(points))
         print('')
@@ -298,6 +465,8 @@ def checkHand(currentPair, currentTable, points):
     if isPair:
         points += pointSystem['pair']
         print('')
+        print('Winning Hand: ' + str(winningHand2))
+        print('')
         print('You got a pair! +' + str(pointSystem['pair']) + ' points')
         print('Total points: ' + str(points))
         print('')
@@ -305,6 +474,8 @@ def checkHand(currentPair, currentTable, points):
 
     elif isHighCard:
         points += pointSystem['highCard']
+        print('')
+        print('Winning Hand: ' + str(winningHand2))
         print('')
         print('You got a high card! +' + str(pointSystem['highCard']) + ' point')
         print('Total points: ' + str(points))
@@ -338,23 +509,50 @@ cardTypes = ['s','c','h','d'] #The suits for a card are: 's' for spades, 'c' for
 cardVals = [str(i) for i in range(2,11)] + valueOrder #The values for a card are numbers 2-10, 'J' for Jack, 'Q' for Queen, 'K' for King, or 'A' for Ace
 cardsList = [s + c for s in cardTypes for c in cardVals] #The list of all 52 possible cards in a deck are compiled into a list
 
-
+'''
 for i in range(300):
     pair = dealPair()
-    print('Dealt hand: ' + str(pair))
-        
     Table = addCards(pair)
-    print('Table cards: ' + str(Table))
-
     points = checkHand(pair, Table, points)
-
-
-
-#testList = ['d10', 'cA', 'h10', 'c2', 'h4', 's10', 'c10']
-
 '''
-testPair = ['cQ', 's10']
-testTable = ['sK', 'c10', 'cA', 'd10', 'cK']
 
-points = checkHand(testPair,testTable,points)
-'''
+#print('Dealt hand: ' + str(pair))
+#print('Table cards: ' + str(Table))
+
+tHighCard = [['d9', 'cJ'], ['d5', 'h4', 'h2', 'sA', 's8']]
+tPair = [['d10', 'c10'], ['c6', 'hA', 'h7', 'd4', 'd3']]
+t2Pair = [['d10', 'c10'], ['c6', 'hA', 'h7', 'd4', 'd6']]
+t3OfKind = [['d10', 'c10'], ['c6', 'h10', 'h7', 'd4', 'd3']]
+tTwo3OfKind = [['d10', 'c10'], ['c7', 'h10', 'h7', 'd7', 'd3']]
+t4OfKind = [['d10', 'c10'], ['c6', 'h10', 'h7', 's10', 'd3']]
+t4OfKind6 = [['d10', 'c10'], ['c6', 'h10', 'h6', 's10', 'd3']]
+t4OfKind7 = [['d10', 'c10'], ['c6', 'h10', 'h6', 's10', 'd6']]
+t3Pair = [['d10', 'c10'], ['c6', 'hA', 'h7', 'd7', 'd6']]
+tFullHouse = [['d10', 'c10'], ['c6', 'h10', 'h7', 's3', 'd3']]
+
+tFlush5 = [['d10', 'cJ'], ['cQ', 'cK', 'h8', 'c2', 'c4']]
+tFlush6 = [['c10', 'cJ'], ['cQ', 'cK', 'h8', 'c2', 'c4']]
+
+tStraight5 = [['d4', 's5'], ['s6', 'd7', 'h8', 'cJ', 'cA']] #_______
+tStraight6 = [['d4', 's5'], ['s6', 'd7', 'h8', 'c9', 'cA']] #_______
+tStraightD1 = [['d5', 's5'], ['s6', 'd7', 'h8', 'c9', 'hJ']] #______
+tStraightD2 = [['d6', 's5'], ['s6', 'd7', 'h8', 'c9', 'hJ']] #______
+tStraight6D1 = [['d5', 's5'], ['s6', 'd7', 'h8', 'c9', 'h10']] #____
+tStraight6D2 = [['d6', 's5'], ['s6', 'd7', 'h8', 'c9', 'h10']] #____
+tStraight6D3 = [['d4', 's5'], ['s6', 'd7', 'h8', 'c9', 'h9']] #_____
+tStraight6T1 = [['d5', 's5'], ['s6', 'd7', 'h8', 'c9', 'h5']] #_____
+tStraight6T2 = [['d8', 's5'], ['s6', 'd7', 'h8', 'c9', 's8']] #_____
+tStraight6T3 = [['d9', 's5'], ['s6', 'd7', 'h8', 'c9', 'h9']] #_____
+tStraight7 = [['d4', 's5'], ['s6', 'd7', 'h8', 'c9', 'c10']] #______
+
+tStraightFlush5 = [['d4', 'd5'], ['d6', 'd7', 'd8', 'cJ', 'cA']]
+tStraightFlush6 = [['d4', 'd5'], ['d6', 'd7', 'd8', 'd9', 'cA']]
+tFakeStraightFlush = [['c4', 'h5'], ['d6', 'd7', 'd8', 'dJ', 'dA']]
+tRoyalFlush = [['d10', 'dJ'], ['dQ', 'dK', 'dA', 'cJ', 'cA']]
+
+testPair = [['d2', 's2'], ['c3', 'h4', 's5', 'h6', 's6']]
+
+points = checkHand(tStraightFlush6[0], tStraightFlush6[1], points)
+
+#print(returnWinningHand(tPair[0], tPair[1], points))
+#print(isStraight(combine(tStraight6T2[0], tStraight6T2[1])[0]))
