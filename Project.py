6bbe62a743a7a9eab90859aecc054b0b4d3ce2cc
CamC7 <cuellarc660@gmail.com>
2019-12-22 02:03:46 -0800

    Put spaces between characters for visibility and also added the png images for the playing card sprites

--- Project.py
+++ Project.py
@@ -18,7 +18,7 @@ win.fill((255,255,255))
 
 class button():
     
-    def __init__(self,color,x,y,width,height,text='',fontColor=(0,0,0),fontSize=60):
+    def __init__(self, color, x, y, width, height, text = '', fontColor = (0,0,0), fontSize = 60):
         
         self.color = color
         self.originalColor = color
@@ -30,20 +30,20 @@ class button():
         self.fontColor = fontColor
         self.fontSize = fontSize
 
-    def draw(self,win,outline=None):
+    def draw(self, win, outline = None):
         
         #Call this method to draw the button on the screen
         if outline:
-            pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
+            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
             
         pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
         
         if self.text != '':
             font = pygame.font.SysFont('comicsans',self.fontSize)
             text = font.render(self.text, 1, self.fontColor)
-            win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+            win.blit(text, (self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
 
-    def isOver(self,pos):
+    def isOver(self, pos):
         
         #Pos is the mouse position or a tuple of (x,y) coordinates
         if pos[0] > self.x and pos[0] < self.x + self.width:
